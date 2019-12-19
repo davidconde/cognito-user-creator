07136c2a4b05b40f88054c94e45a35beb0e9b5eb
davidcondemarin <david.conde.marin@gmail.com>
2019-12-19 09:32:26 +0000

    added command configs

---
+++ commands-config.js
@@ -0,0 +1,11 @@
+const options = [
+    { "name": "command", "alias": "c", "type": String },
+    { "name": "verbose", "alias": "v", "type": Boolean },
+    { "name": "user", "alias": "e", "type": String },
+    { "name": "userPoolId", "alias": "u", "type": String },
+    { "name": "clientId", "alias": "i", "type": String },
+    { "name": "password", "alias": "p", "type": String },
+    { "name": "region", "alias": "r", "type": String }
+]
+
+module.exports = options;
